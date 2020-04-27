a08453172a880f3f26826f21483bde77f48f1506
Khiem Tran <gia_khiem@live.com>
2020-04-27 18:48:44 +0700

    CRUD Task API

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,49 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Patch,
+  Query,
+} from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task, TaskStatus } from './task.model';
+import { GetTasksFilterDTO } from './dto/get-tasks-filter-dto';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private taskService: TasksService) {}
+
+  @Get()
+  getTasks(@Query() filterDto: GetTasksFilterDTO): Task[] {
+    if (Object.keys(filterDto).length) {
+      return this.taskService.getTaskWithFilters(filterDto);
+    }
+    return this.taskService.getAllTasks();
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.taskService.getTaskById(id);
+  }
+
+  @Post()
+  createTask(@Body() createTaskDto): Task {
+    return this.taskService.createTask(createTaskDto);
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id: string): void {
+    this.taskService.deleteTask(id);
+  }
+
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body('status') status: TaskStatus,
+  ): Task {
+    return this.taskService.updateTaskStatus(id, status);
+  }
+}
